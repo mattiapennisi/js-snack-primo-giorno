dbf1cac391592f7ac6c452cb826811b87a5f961b
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-13 11:23:32 +0100

    edit texts

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,4 +1,15 @@
 /*
-
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
+let wordOne = String(prompt('Enter a word'))
+let wordTwo = String(prompt('Enter a second word'))
+
+if (wordOne.length > wordTwo.length) {
+    console.log(`${wordOne} is longer than ${wordTwo}`);
+} else if (wordOne.length < wordTwo.length) {
+    console.log(`${wordTwo} is longer than ${wordOne}`);
+} else {
+    console.log('Words got the same length');
+}
