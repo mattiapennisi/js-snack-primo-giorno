35384a49c040ec02e1deb14089be18c3c54b5534
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-13 10:46:44 +0100

    edit script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
 
-const numOne = prompt('Inserisci un numero (esempio "1"')
-const numTwo = prompt('Inserisci un altro numero (esempio "1"')
+const numOne = Number(prompt('Inserisci un numero (esempio "1")'))
+const numTwo = Number(prompt('Inserisci un altro numero (esempio "1")'))
 
 if (numOne > numTwo) {
     console.log(numOne);
