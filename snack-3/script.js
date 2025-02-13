cdbf618578c5ce85bdfcc350ccd92257a0c9ba7d
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-13 11:52:38 +0100

    add snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,3 +1,13 @@
 /*
-
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
 */
+
+numbers = 0
+
+for (let i = 1; i < 11; i++) {
+    let num = Number(prompt('Enter a number'))
+    numbers += num
+}
+
+console.log(numbers);
